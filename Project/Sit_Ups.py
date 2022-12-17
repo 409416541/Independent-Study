0c5c7c9a645eb833a2f6ede2cf1697764d24606e
409416541 <80378539+409416541@users.noreply.github.com>
2022-12-17 20:33:38 +0800

    Update Sit_Ups.py

--- Project/Sit_Ups.py
+++ Project/Sit_Ups.py
@@ -50,7 +50,7 @@ def Pose_Detected(cap):
                 Global_Use.thecount(img, str(int(count)))
 
             return img
-            #cv2.imshow("landmarks", img)
+            #cv2.imshow("Sit Ups", img)
 
         else:
             break
