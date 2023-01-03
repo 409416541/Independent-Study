e3928f2da8f2dc41a441f04898e8bad914f3b2f9
pleaseletmesleep <joeljtatang@gmail.com>
2023-01-04 00:58:26 +0800

    Update Interface.py

--- Project/Interface.py
+++ Project/Interface.py
@@ -33,7 +33,7 @@ difficulty = [8, 15, 25]
 text = ['Please choose action', 
         '1. Sit Ups', '2. Push Up', '3. Squat', '4. Leg Raises', '5. Jumping Jacks', '6. Break', # interface
         'Your choose is ', 
-        'Rock. OK', 'Pink. NO',
+        'Rock. OK', 'Pinky. NO',
         'Please choose you want', 
         '1. Sample Video', '2. Go Sport', '3. back to interface',
         'How times you want', 
