f9a8115addef98ceae7075606a9d0141a0b51ca8
409410197 <409410197@gms.tku.edu.tw>
2023-01-18 14:57:25 +0800

    半成品

--- Project/Sit_Ups.py
+++ Project/Sit_Ups.py
@@ -39,19 +39,31 @@ def Pose_Detected(cap, use_vedio, dir, count):
             landmarks, img = detector.findPose(img, draw=True)
 
             if landmarks:
-                angle, img = detector.findAngle(landmarks[12], landmarks[24],
+                angle1_1, img = detector.findAngle(landmarks[12], landmarks[24],
                                                 landmarks[26], img)
-
+                angle1_2, img = detector.findAngle(landmarks[11], landmarks[23],
+                                                landmarks[25], img)
+                angle2_1, img = detector.findAngle(landmarks[12], landmarks[24],
+                                                landmarks[26], img)
+                angle2_2, img = detector.findAngle(landmarks[11], landmarks[23],
+                                                landmarks[25], img)
+                angle3_1, img = detector.findAngle(landmarks[24], landmarks[26],
+                                                landmarks[28], img)
+                angle3_2, img = detector.findAngle(landmarks[23], landmarks[25],
+                                                landmarks[27], img)
+                angle4_1, img = detector.findAngle(landmarks[12], landmarks[14],
+                                                landmarks[16], img)
+                angle4_2, img = detector.findAngle(landmarks[11], landmarks[13],
+                                                landmarks[15], img)
                 # 顯示進度條
-                Global_Use.thebar(img, angle, 20, 100)
-
-                if angle <= 50:   # 目前狀態:起坐
-                    if dir == 0:  # 之阱狀態:仰臥
+                Global_Use.thebar(img, angle1_1, 20, 100)
+                
+                if dir == 0:  # 之阱狀態:仰臥
+                    if angle1_1 <= 50:   # 目前狀態:起坐
                         count = count + 0.5
                         dir = 1   # 更新狀態:起坐
-
-                if angle >= 90:   # 目前狀態:仰臥
-                    if dir == 1:  # 之前狀態:起坐
+                if dir == 1:  # 之前狀態:起坐
+                    if angle1_1 >= 90:   # 目前狀態:仰臥
                         count = count + 0.5
                         dir = 0   # 更新狀態:仰臥
 
