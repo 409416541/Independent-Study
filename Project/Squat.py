8532c31c05daddf0c0a892e726a99bb4162745ca
FreeBear1003 <409411039@gms.tku.edu.tw>
2023-02-11 16:54:40 +0800

    squat angle

--- Project/Squat.py
+++ Project/Squat.py
@@ -39,21 +39,44 @@ def Pose_Detected(cap, use_vedio, dir, count):
             landmarks, img = detector.findPose(img, draw=True)
 
             if landmarks:
-                angle, img = detector.findAngle(landmarks[24], landmarks[26],
+                #angle1:肩膀到髖到膝蓋的角度
+                angle1_1, img = detector.findAngle(landmarks[12], landmarks[24],
+                                                landmarks[26], img)
+                angle1_2, img = detector.findAngle(landmarks[11], landmarks[23],
+                                                landmarks[25], img)
+                #angle2:髖到膝蓋到腳踝的角度
+                angle2_1, img = detector.findAngle(landmarks[24], landmarks[26],
                                                 landmarks[28], img)
+                angle2_2, img = detector.findAngle(landmarks[23], landmarks[25],
+                                                landmarks[27], img)
+                '''
+                #angle3:髖到髖到膝蓋的角度
+                angle3_1, img = detector.findAngle(landmarks[26], landmarks[24],
+                                                landmarks[23], img)
+                angle3_2, img = detector.findAngle(landmarks[24], landmarks[23],
+                                                landmarks[25], img)
+                '''
                                                 
                 # 顯示進度條
-                Global_Use.thebar(img, angle, 95, 175)
+                Global_Use.thebar(img, angle2_1, 90, 175)
+                
+                if 70 <= angle1_1 <= 180 and 70 <= angle1_2 <= 180 \
+                    and 80 <= angle2_1 <= 180 and 80 <= angle2_2 <= 180 :
+                    '''
+                    and 60 <= angle3_1 <= 170 and 60 <= angle3_2 <= 175:
+                    '''
 
-                if angle <= 110:  # 目前狀態:蹲下
                     if dir == 0:  # 之前狀態:站起
-                        count = count + 0.5
-                        dir = 1   # 更新狀態:蹲下
+                        if 70 <= angle1_1 <=110 and 70 <= angle1_2 <= 110 \
+                            and 80 <= angle2_1 <= 110 and 80 <= angle2_2 <= 110:  # 目前狀態:蹲下
+                            count = count + 0.5
+                            dir = 1   # 更新狀態:蹲下
 
-                if angle >= 165:  # 目前狀態:站起
                     if dir == 1:  # 之前狀態:蹲下
-                        count = count + 0.5
-                        dir = 0   # 更新狀態:站起
+                        if 155 <= angle1_1 <=180 and 155 <= angle1_2 <= 180 \
+                            and 160 <= angle2_1 <= 180 and 160 <= angle2_2 <= 180:  # 目前狀態:站起
+                            count = count + 0.5
+                            dir = 0   # 更新狀態:站起
                         
                 Global_Use.thecount(img, str(int(count)))
 
@@ -73,4 +96,3 @@ def Pose_Detected(cap, use_vedio, dir, count):
     cv2.destroyAllWindows()
 
 #Pose_Detected(cap, 1, dir , count)
-#Pose_Detected(cap, 0, dir , count)
