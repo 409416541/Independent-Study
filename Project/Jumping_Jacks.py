f0042e7780c00d246315f3e26c1d58cf36039cc7
pleaseletmesleep <joeljtatang@gmail.com>
2023-02-27 00:54:39 +0800

    jumping jacks sound new

--- Project/Jumping_Jacks.py
+++ Project/Jumping_Jacks.py
@@ -3,7 +3,6 @@ import Global_Use
 import cv2
 import winsound
 import pygame  
-
 '''
 cap = cv2.VideoCapture('./Project/Test_Media/Jumping_Jacks.mp4')
 
@@ -18,7 +17,7 @@ if not cap.isOpened():
     exit()
 '''
 
-dir = 1  # 0: 開 1: 合
+dir = 0  # 0: 開 1: 合
 count = 0
 
 def Pose_Detected(cap, use_vedio, dir, count, text):
@@ -74,29 +73,29 @@ def Pose_Detected(cap, use_vedio, dir, count, text):
                         accuracy3 = 100
 
                         # 目前狀態::開
-                        if dir == 0: # 之前狀態:close
+                        if dir == 1: # 之前狀態:close
                             if 70 <= angle2_1 <= 90 and 0 <= angle1_1 <= 30\
                             and 170 <= angle3_1 <= 180 \
                             and 70 <= angle2_2 <= 90 and 0 <= angle1_2 <= 30\
                             and 170 <= angle3_2 <= 180:
                                 count = count + 0.5
-                                dir = 1    # 更新狀態:合
-                                                                    
+                                dir = 0    # 更新狀態:合
+                                if count % 1 == 0:
+                                    pygame.mixer.init()
+                                    pygame.mixer.music.load('./Project/Test_Media/sound.wav')
+                                    pygame.mixer.music.play()
+                                   #winsound.PlaySound("./Project/Test_Media/sound.wav", winsound.SND_ASYNC | winsound.SND_ALIAS )                                   
                         # 目前狀態::合
-                        if dir == 1: # 之前狀態:open
+                        if dir == 0: # 之前狀態:open
 
                             if 90 <= angle2_1 <= 125 and 150 <= angle1_1 <= 180\
                             \
                             and 90 <= angle2_2 <= 125 and 150 <= angle1_2 <= 180\
                             :
                                 count = count + 0.5
-                                dir = 0    # 更新狀態:開
+                                dir = 1    # 更新狀態:開
 
-                                if count % 1 == 0:
-                                    pygame.mixer.init()
-                                    pygame.mixer.music.load('./Project/Test_Media/sound.wav')
-                                    pygame.mixer.music.play()
-                                   #winsound.PlaySound("./Project/Test_Media/sound.wav", winsound.SND_ASYNC | winsound.SND_ALIAS )
+                               
 
                 else:
                     if angle2_1 < 70:
